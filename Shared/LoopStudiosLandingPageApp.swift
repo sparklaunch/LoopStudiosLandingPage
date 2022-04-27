63f466a69c84742c244c49a2f40f827a93a42d99
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-27 10:44:16 +0900

    Initial Commit

---
+++ Shared/LoopStudiosLandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopStudiosLandingPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct LoopStudiosLandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
